293face3fce0b4d6dd56a1e5c5577dbd0f273a45
Anthony Salemo <ajssalemo@gmail.com>
2020-11-05 22:24:06 -0500

    added some styling/momentjs so the UI is easier on the eyes

--- NextJSFrontEnd/pages/index.js
+++ NextJSFrontEnd/pages/index.js
@@ -1,24 +1,33 @@
 import Head from "next/head";
+import moment from "moment"
 import { Fragment, useState, useEffect } from "react";
 
 const Home = ({ data }) => {
   const [weather, getWeather] = useState(null);
   useEffect(() => {
-    getWeather(data)
-  }, [])
+    getWeather(data);
+  }, []);
   return (
     <Fragment>
       <Head>
         <title>Docker-compose proof-of-concept</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="bg-gray-800 h-screen text-white">
+      <div className="bg-gray-800 h-auto text-white">
         <div className="text-center pt-16">
+          <h1 className="text-4xl font-bold">Weather Descriptions</h1>
           {!weather
             ? "Undefined"
             : weather.map((forecast, i) => (
-                <ul>
-                  <li key={i}>{forecast.summary}</li>
+                <ul key={i} className="py-12">
+                  <li key={i} className="flex flex-col justify-center">
+                    <b>Today's date: </b>
+                    {moment(forecast.date).format("dddd, MMMM Do YYYY")}
+                    <b>Temperature(C): </b>
+                    {forecast.temperatureC}&deg;C
+                    <b>Temperature(F): </b>
+                    {forecast.temperatureF}&deg;F
+                  </li>
                 </ul>
               ))}
         </div>
